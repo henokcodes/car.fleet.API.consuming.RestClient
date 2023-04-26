4ea0a2ff926807ddf609ba7630ed25ac8caf889d
henok <henokcodes@gmail.com>
2023-04-25 21:59:07 -0500

    Initial commit

---
+++ src/main/java/com/henokcodes/carfleetclient/CarfleetclientApplication.java
@@ -0,0 +1,13 @@
+package com.henokcodes.carfleetclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarfleetclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarfleetclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henokcodes/carfleetclient/CarfleetclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henokcodes.carfleetclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarfleetclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
