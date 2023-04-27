ac5c229d6de465bcd9dcf1cb959fb890406d7dff
henok <henokcodes@gmail.com>
2023-04-27 08:04:19 -0500

    Car and Cars domain

---
+++ src/main/java/com/henokcodes/carfleetclient/Cars.java
@@ -0,0 +1,28 @@
+package com.henokcodes.carfleetclient;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+
+public class Cars {
+	private List<Car> cars;
+	
+
+	public Cars() {
+		cars = new ArrayList<Car>();
+	}
+
+	public Cars(List<Car> cars) {
+		this.cars = cars;
+	}
+
+	public List<Car> getCars() {
+		return cars;
+	}
+
+	public void setCars(List<Car> cars) {
+		this.cars = cars;
+	}
+		
+}
